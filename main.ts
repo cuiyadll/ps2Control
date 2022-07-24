7e563db3adfd9e2a60af4dc6c06bd3a1bf39ff82
cuiyadll <87569571+cuiyadll@users.noreply.github.com>
2022-07-24 12:01:13 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
+function 高速前进 () {
+    makerobo.MotorRunDual(
+    makerobo.Motors.Left,
+    255,
+    makerobo.Motors.Right,
+    255
+    )
+}
 function 原地右转 () {
     makerobo.MotorRunDual(
     makerobo.Motors.Left,
@@ -33,20 +41,60 @@ function 左转 () {
 function 舵机转动 (jiaodu: number) {
     makerobo.Servo(makerobo.Servos.S1, 90 + jiaodu)
 }
+function 高速原地左转 () {
+    makerobo.MotorRunDual(
+    makerobo.Motors.Left,
+    -255,
+    makerobo.Motors.Right,
+    255
+    )
+}
+function 高速后退 () {
+    makerobo.MotorRunDual(
+    makerobo.Motors.Left,
+    -255,
+    makerobo.Motors.Right,
+    -255
+    )
+}
+function 高速右转 () {
+    makerobo.MotorRunDual(
+    makerobo.Motors.Left,
+    255,
+    makerobo.Motors.Right,
+    0
+    )
+}
 function 后退 () {
     makerobo.MotorRunDual(
     makerobo.Motors.Left,
-    -200,
+    -150,
     makerobo.Motors.Right,
-    -200
+    -150
+    )
+}
+function 高速左转 () {
+    makerobo.MotorRunDual(
+    makerobo.Motors.Left,
+    0,
+    makerobo.Motors.Right,
+    255
+    )
+}
+function 高速原地右转 () {
+    makerobo.MotorRunDual(
+    makerobo.Motors.Left,
+    255,
+    makerobo.Motors.Right,
+    -255
     )
 }
 function 前进 () {
     makerobo.MotorRunDual(
     makerobo.Motors.Left,
-    200,
+    150,
     makerobo.Motors.Right,
-    200
+    150
     )
 }
 function 停止 () {
@@ -110,22 +158,26 @@ basic.forever(function () {
         basic.showIcon(IconNames.No)
         原地左转()
         basic.pause(5000)
-    } else if (ps2controller.button_pressed(ps2controller.PS2Button.L1) == 1 && ps2controller.button_pressed(ps2controller.PS2Button.L2) == 1) {
-        左转()
-    } else if (ps2controller.button_pressed(ps2controller.PS2Button.R1) == 1 && ps2controller.button_pressed(ps2controller.PS2Button.R2) == 1) {
-        右转()
     } else if (ps2controller.button_pressed(ps2controller.PS2Button.Start) == 1) {
         舵机转动(90)
         basic.pause(500)
         舵机转动(-90)
         basic.pause(500)
         舵机转动(0)
-    } else if (ps2controller.button_pressed(ps2controller.PS2Button.Analog_Right) == 1) {
-        前进()
-        makerobo.MotorRun(makerobo.Motors.Centre, 255)
+    } else if (ps2controller.button_pressed(ps2controller.PS2Button.R1) == 1 && ps2controller.button_pressed(ps2controller.PS2Button.Up) == 1) {
+        高速前进()
+    } else if (ps2controller.button_pressed(ps2controller.PS2Button.R1) == 1 && ps2controller.button_pressed(ps2controller.PS2Button.Down) == 1) {
+        高速后退()
+    } else if (ps2controller.button_pressed(ps2controller.PS2Button.R1) == 1 && ps2controller.button_pressed(ps2controller.PS2Button.Left) == 1) {
+        高速左转()
+    } else if (ps2controller.button_pressed(ps2controller.PS2Button.R1) == 1 && ps2controller.button_pressed(ps2controller.PS2Button.Right) == 1) {
+        高速右转()
+    } else if (ps2controller.button_pressed(ps2controller.PS2Button.R1) == 1 && ps2controller.button_pressed(ps2controller.PS2Button.Cross) == 1) {
+        高速原地左转()
+    } else if (ps2controller.button_pressed(ps2controller.PS2Button.R1) == 1 && ps2controller.button_pressed(ps2controller.PS2Button.Circle) == 1) {
+        高速原地右转()
     } else {
         TM1650.showNumber(1)
         停止()
-        makerobo.MotorRun(makerobo.Motors.Centre, 0)
     }
 })
